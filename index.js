06eca8fbc92bf0222f24879cf58a056e92028dd9
nguyen2966 <167694401+nguyen2966@users.noreply.github.com>
2025-07-06 09:11:27 +0700

    add .gitignore and .env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const route = require("./routes/client/index.route")
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 app.set("views","./views")
 app.set("view engine","pug")
@@ -11,7 +11,7 @@ app.set("view engine","pug")
 route(app)
 
 app.listen(port,()=>{
-    console.log("Example app listening on port "+port)
+    console.log("Example app listening on port "+ port)
 })
 
  
