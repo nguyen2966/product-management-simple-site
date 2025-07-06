1a1ad8fb547d12649df50826f8e7f0988b75de44
nguyen2966 <167694401+nguyen2966@users.noreply.github.com>
2025-07-06 11:13:25 +0700

    Add database

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 require('dotenv').config() //Line nay de add cau hình từ .env
 const route = require("./routes/client/index.route")
 
+const database = require("./config/database")
+database.connect()
+
 const app = express()
 const port = process.env.PORT
 
@@ -9,6 +12,8 @@ app.set("views","./views")
 app.set("view engine","pug")
 
 app.use(express.static("public"))//nhúng file tĩnh
+
+
 
 route(app)
 
