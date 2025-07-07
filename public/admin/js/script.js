cf73ca3a805cbe1c652f690f768aff69a6ab2054
nguyen2966 <167694401+nguyen2966@users.noreply.github.com>
2025-07-07 23:01:15 +0700

    add filter feature to Admin UI

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -15,3 +15,20 @@ if(buttonStatus.length > 0 ){
         })
     })
 }
+
+
+//Tìm kiếm
+const formSearch = document.querySelector("#form-search")
+if(formSearch){
+    let url = new URL(window.location.href)
+    formSearch.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const keyword = e.target.keyword.value
+        if(keyword){
+            url.searchParams.set("keyword",keyword)
+        }else{
+            url.searchParams.delete("keyword")
+        }
+        window.location.href = url.href
+    })
+}
