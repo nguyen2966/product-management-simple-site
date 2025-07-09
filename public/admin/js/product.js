42348a436e97e6dbb5faf69a76de9bf4032b51bd
nguyen2966 <167694401+nguyen2966@users.noreply.github.com>
2025-07-09 15:41:18 +0700

    Add delete feature for Admin

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -18,3 +18,23 @@ if(buttonChangeStatus.length>0){
         })
     })
 }
+
+//Delete items
+const deleteButtons = document.querySelectorAll("[button-delete]")
+if(deleteButtons.length>0){
+    const formDeleteItem = document.querySelector("#form-delete-item")
+    const path = formDeleteItem.getAttribute("data-path")
+    deleteButtons.forEach(button=>{
+       button.addEventListener("click",()=>{
+          const isConfirm = confirm("Bạn có chắc muốn xóa sản phẩm này")
+          if(isConfirm){
+            const id = button.getAttribute("data-id")
+
+            const action = `${path}/${id}?_method=DELETE`
+            formDeleteItem.action = action
+            formDeleteItem.submit()
+          }
+       })
+    })
+}
+//End delete items
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -9,4 +9,6 @@ router.get("/",controller.index)
 router.patch("/change-status/:status/:id",controller.changeStatus)
 
 router.patch("/change-multi",controller.changeMulti)
+
+router.delete("/delete/:id",controller.deleteItem)
 module.exports = router
