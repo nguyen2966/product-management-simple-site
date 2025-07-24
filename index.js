7dc4db9b8260976549b57af8558f8d6a3c8c559f
unknown <nguyentoankhoa12@gmail.com>
2025-07-24 14:47:08 +0700

    Add basic MCE feature for admin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const cookieParser = require("cookie-parser")
@@ -26,6 +27,10 @@ app.use(cookieParser("NLN"))
 app.use(session({cookie:{maxAge: 60000}}))
 app.use(flash())
 //End Flash
+
+//TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+//End TinyMCE
 
 app.set("views",`${__dirname}/views`)
 app.set("view engine","pug")
---
+++ public/admin/js/tinyMCEconfig.js
@@ -0,0 +1,5 @@
+tinymce.init({
+  selector: 'textarea.text-area-mce'
+});
+
+// Find textarea elements with class text-area-mce
